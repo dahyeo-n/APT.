027a961d803c0892acb9229c1360935db72475a0
Dahyeon Jin <wlsekgus12@gmail.com>
2024-10-29 08:20:54 +0900

    pnpm, TypeScript, Next.js, Supabase, Tanstack Query, Tailwind CSS 및 Recoil을 사용한 초기 프로젝트 설정

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}', // Next.js 파일들이 위치한 경로 설정
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
