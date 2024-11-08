acce4aab84b20e9c0a723fce12ae0e021e6037b5
Dahyeon Jin <wlsekgus12@gmail.com>
2024-11-08 23:04:41 +0900

    Chore: NextUI 스타일이 Vercel 배포 시 누락되는 문제를 해결하기 위해 next-transpile-modules을 사용하여 NextUI 관련 모듈을 트랜스파일

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+// next.config.js
+const withTM = require('next-transpile-modules')(['@nextui-org/theme']);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  optimizeFonts: false,
+const nextConfig = withTM({
   experimental: {
     optimizeCss: false,
   },
-};
+  optimizeFonts: false,
+});
 
 module.exports = nextConfig;
