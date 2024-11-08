16bee9fe65bdf53d82d413cf3af694140d50b53f
Dahyeon Jin <wlsekgus12@gmail.com>
2024-11-09 01:20:57 +0900

    Chore: next.config.js에 CSS 파일이나 정적 리소스에 대해 캐시 버스트 적용

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+module.exports = {
+  generateBuildId: async () => {
+    return new Date().toISOString();
+  },
+};
